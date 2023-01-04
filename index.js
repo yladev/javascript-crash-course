21282c20763a8460191094456c365bf6bba8a69c
yladev <yanlinnaung881@gmail.com>
2023-01-04 17:02:31 +0630

    initilization-variables

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("hello world");
+
+var number = 5; //in-line comment
+
+number = 9;
+
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Jason"
+myName = 8
+let ourName = "JasonAndrew"
+const pi = 3.14
+
+
+var a = 2;
+var b = 2;
+console.log(a + b)
+
+a = 7;
+b = a;
+console.log(a)
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a ";
+
+// Do not change code below this line
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+console.log(a, b, c)
