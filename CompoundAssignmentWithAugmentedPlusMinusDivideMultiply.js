ee680fa0149da351e4d89c30521f085a0062d650
yladev <yanlinnaung881@gmail.com>
2023-01-05 17:54:23 +0630

    Compound Assignment With Augmented

---
+++ CompoundAssignmentWithAugmentedPlusMinusDivideMultiply.js
@@ -0,0 +1,50 @@
+var a = 3;
+var b = 4;
+var c = 6;
+
+a += 2;
+b += 6;
+c += 9;
+
+console.log("this is a",a);
+console.log("this is b",b);
+console.log("this is c",c);
+
+//Compound Assignment With Augmented Subtraction
+var d = 1000;
+var e = 2000;
+var f = 5000;
+
+d -= 500;
+e -= 200;
+f -= 2000;
+
+console.log("this is d",d);
+console.log("this is e",e);
+console.log("this is f",f);
+
+//Compound Assignment With Augmented Multiplication
+var g = 10;
+var h = 30;
+var i = 40;
+
+g *= 5;
+h *= 9;
+i *= 7;
+
+console.log("this is g",g);
+console.log("this is h",h);
+console.log("this is i",i);
+
+//Compound Assignment With Augmented Division
+var j = 9;
+var k = 20;
+var l = 30;
+
+j /= 3;
+k /= 5;
+l /= 10;
+
+console.log("this is j",j);
+console.log("this is k",k);
+console.log("this is l",l);
