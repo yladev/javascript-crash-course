e8d217a7cac9ec0749164a3eed2bc3fd8681157e
yladev <yanlinnaung881@gmail.com>
2023-01-07 17:46:04 +0630

    escape sequences in strings

---
+++ EscapeSequencesInStrings.js
@@ -0,0 +1,21 @@
+/***
+Code Output
+\' single quote
+\" double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f from feed
+***/
+
+
+
+
+
+
+
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+console.log(myStr);
