554c00a75f7773784cd16769f625a578a0f64d6f
yladev <yanlinnaung881@gmail.com>
2023-01-07 17:32:31 +0630

    string-with doube quotes and single quote

---
+++ EscapingLiteralQuotesInStrings.js
@@ -0,0 +1,2 @@
+ var Str = ("My name is Yan Linn Aung. \" I am 17 years old. \" I am a student.");
+ console.log(Str);
---
+++ QuotingStringsWithSingleQuotes.js
@@ -0,0 +1,2 @@
+var str = 'Now I am attending in Level-3 Diploma in computing';
+console.log(str);
