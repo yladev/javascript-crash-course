c654215283eb88195f05811a2e2e22988f21ae1e
yladev <yanlinnaung881@gmail.com>
2023-01-08 17:08:43 +0630

    update-word blank

--- BracketNotationToFindNthCharInString.js
+++ BracketNotationToFindNthCharInString.js
@@ -3,7 +3,7 @@ var secondLetterOfFirstName = firstName[1];
 console.log(firstName);
 console.log(firstName[1]);
 
-var lastName = "May";
+var lastName = "Andrew";
 var thirdLetterOflastName = lastName[2];
 console.log(lastName);
 console.log(lastName[2]);
