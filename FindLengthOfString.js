dcca433f8dfd1b6c5c859e9cb3bb09abbce34949
yladev <yanlinnaung881@gmail.com>
2023-01-08 10:16:54 +0630

    update-find length of string

---
+++ FindLengthOfString.js
@@ -0,0 +1,9 @@
+ var firstNameLength = 0;
+ var firstName = "Jason";
+firstNameLength = firstName.length
+console.log(firstName.length);
+
+var secondNameLength = 0;
+var secondName = "Andrew";
+secondNameLength = secondName.length;
+console.log(secondName.length);
