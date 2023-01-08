9d5b576be55ba6f21879713536bf5c32fdb70140
yladev <yanlinnaung881@gmail.com>
2023-01-08 12:28:37 +0630

    update-bracket notation to find the last char in string

---
+++ BracketNotationToFindLastCharInString.js
@@ -0,0 +1,7 @@
+var firstName = "Jason";
+var lastLetterOfFirstName = firstName[firstName.length -1];
+console.log(lastLetterOfFirstName);
+
+var lastName = "Andrewww";
+var lastLetterOfLastName = lastName[lastName.length -1];
+console.log(lastLetterOfLastName);
