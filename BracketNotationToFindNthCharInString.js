c9421afd0c4c8a27f867d861a303da67d898aeea
yladev <yanlinnaung881@gmail.com>
2023-01-08 12:16:22 +0630

    edit-a little

--- BracketNotationToFindNthCharInString.js
+++ BracketNotationToFindNthCharInString.js
@@ -1,5 +1,9 @@
 var firstName = "Jason";
 var secondLetterOfFirstName = firstName[1];
+console.log(firstName);
+console.log(firstName[1]);
 
 var lastName = "May";
 var thirdLetterOflastName = lastName[2];
+console.log(lastName);
+console.log(lastName[2]);
