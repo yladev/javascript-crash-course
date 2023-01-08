fc495d826cf05615b2e204771db1810db8ea6b70
yladev <yanlinnaung881@gmail.com>
2023-01-08 17:19:32 +0630

    add-word blank in command line

--- WordBlank.js
+++ WordBlank.js
@@ -1,8 +1,11 @@
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    //Your code below this line
     var result = "";
     result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb;
+    //Your code above this line
     return result;
 }
 
+//Change the words here to last your function
 console.log(wordBlanks("dog", "big", "ran", "quickly"));
 console.log(wordBlanks("bike", "slow", "flew", "slowly"));
