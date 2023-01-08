ed919b6e93628d58837314d5964572021a6a2f89
yladev <yanlinnaung881@gmail.com>
2023-01-08 10:35:30 +0630

    update-bracket notation to find the first char of string

---
+++ BracketNotationToFindFirstCharInString.js
@@ -0,0 +1,9 @@
+var firstLetterOfFirstName = "";
+var firstName = "Jason";
+firstLetterOfFirstName = firstName[0];
+console.log(firstLetterOfFirstName);
+
+var lastLetterOfLastName = "";
+var lastName = "Andrew"
+lastLetterOfLastName = lastName[0];
+console.log(lastLetterOfLastName);
