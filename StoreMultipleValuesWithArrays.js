44f9235e9d77249da2418f2fb743b7a72a3f1395
yladev <yanlinnaung881@gmail.com>
2023-01-08 17:45:31 +0630

    update-store multiple values with arrays

---
+++ StoreMultipleValuesWithArrays.js
@@ -0,0 +1,8 @@
+var ourArray = ["john", 23]; 
+console.log(ourArray); //Length:2 Arry No:2
+
+var myArray = ["Andrew", 1, 2, 3, 4, 5, 6]; 
+console.log(myArray); //Length:7 Array No:7
+
+var yourArray = ["Apple", "Orange", "Mango", "Banana", "Pineapple", "Tomato"];
+console.log(yourArray); //Length:6 Array No:6
