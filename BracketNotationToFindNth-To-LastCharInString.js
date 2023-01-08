5d39ed1fc27d4d336b673506002329f787266e36
yladev <yanlinnaung881@gmail.com>
2023-01-08 12:38:31 +0630

    update-bracket notation to find nth-to-last char in string

---
+++ BracketNotationToFindNth-To-LastCharInString.js
@@ -0,0 +1,7 @@
+var firstName = "yla";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+console.log(thirdToLastLetterOfFirstName);
+
+var lastName = "yladev";
+var secondToLastLetterOfLastName = lastName[lastName.length -2];
+console.log(secondToLastLetterOfLastName);
