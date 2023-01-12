c81955a83377e609b53ed0c9942065d5bdd2de3e
yladev <yanlinnaung881@gmail.com>
2023-01-12 21:05:04 +0630

    update-manipulate arrays with pop

--- ManipulateArraysWithPop.js
+++ ManipulateArraysWithPop.js
@@ -1,8 +1,13 @@
 // pop()
 var ourArray = [1,2,3];
-var remoteFromOurArray = ourArray.pop();
+var removeFromOurArray = ourArray.pop();
 console.log(ourArray);
 
 var myArray = [["Jason", 17], ["Andrew", 17]];
-var remoteFrommyArray = myArray.pop();
+var removeFrommyArray = myArray.pop();
 console.log(myArray);
+
+var names = ["yan", "thuta", "san", "moe"];
+var removeName = names.pop();
+console.log(names);
+console.log(removeName); // unmodified array
