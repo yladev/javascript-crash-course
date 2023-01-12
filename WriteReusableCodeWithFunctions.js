11dd6d00be0f1787feac08218ea1aea70b47816e
yladev <yanlinnaung881@gmail.com>
2023-01-12 23:25:28 +0630

    update-write reusable code with functions

---
+++ WriteReusableCodeWithFunctions.js
@@ -0,0 +1,10 @@
+// should always recall the function name 
+function myReusableFunction() {
+    console.log("Hello, World");
+}
+myReusableFunction(); // recall the function name
+
+function reusableFunction() {
+    console.log("Hi, World");
+}
+reusableFunction(); // recall the function name
