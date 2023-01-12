c7cdb5c668f57a0c995f89c7e95cbafacae23024
yladev <yanlinnaung881@gmail.com>
2023-01-13 01:18:38 +0630

    Update-Passing Values To Functions With Arguments

---
+++ PassingValuesToFunctionsWithArguments.js
@@ -0,0 +1,10 @@
+function ourFunctionWithArguments(a, b) {
+    console.log("subtraction two numbers use function with arguments =>", a -b);
+}
+ourFunctionWithArguments(12, 11);
+
+
+function myArguments(numOne, numTwo) {
+    console.log("addition two numbers use function with arguments =>", numOne + numTwo);
+}
+myArguments(100, 200)
