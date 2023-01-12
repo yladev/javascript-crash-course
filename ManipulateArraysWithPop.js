c36437d01ca65792654aaf2b82a34021e98b660d
yladev <yanlinnaung881@gmail.com>
2023-01-12 18:07:50 +0630

    update-manipulate arrays with pop

---
+++ ManipulateArraysWithPop.js
@@ -0,0 +1,8 @@
+// pop()
+var ourArray = [1,2,3];
+var remoteFromOurArray = ourArray.pop();
+console.log(ourArray);
+
+var myArray = [["Jason", 17], ["Andrew", 17]];
+var remoteFrommyArray = myArray.pop();
+console.log(myArray);
