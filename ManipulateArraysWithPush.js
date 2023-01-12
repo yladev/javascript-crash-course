8c38bab1e26046652e31532e744fe7775ef7bdf3
yladev <yanlinnaung881@gmail.com>
2023-01-12 17:40:56 +0630

    update-manipulate arrays with push

---
+++ ManipulateArraysWithPush.js
@@ -0,0 +1,10 @@
+var myArray = [["Andrew", 23], ["Jason", 11]];
+myArray.push(["Yan", 17]);
+console.log(myArray);
+
+var ourArray = [
+    ["Yan", "Linn", "Aung"],
+    [23, 11, 2005]
+];
+ourArray.push(["KMD", "Institute"]);
+console.log(ourArray);
