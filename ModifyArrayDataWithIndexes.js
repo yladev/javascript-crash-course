7764138c227a90336def7bb498d5547c2bf6ce6e
yladev <yanlinnaung881@gmail.com>
2023-01-13 08:50:46 +0630

    edit-passing values to functions with arguments

--- ModifyArrayDataWithIndexes.js
+++ ModifyArrayDataWithIndexes.js
@@ -1,7 +1,7 @@
 var myArray = [50, 70, 90];
 myArray[1] = 100;
-console.log("this is modify array data of [2]", myArray);
+console.log("this is modify array data of [2] =>", myArray);
 
 ourArray = [10, 20, 50];
 ourArray[0] = 12500;
-console.log("this is modify array data of [0]", ourArray);
+console.log("this is modify array data of [0] =>", ourArray);
--- PassingValuesToFunctionsWithArguments.js
+++ PassingValuesToFunctionsWithArguments.js
@@ -1,10 +1,21 @@
 function ourFunctionWithArguments(a, b) {
     console.log("subtraction two numbers use function with arguments =>", a -b);
 }
-ourFunctionWithArguments(12, 11);
+ourFunctionWithArguments(9000, 8763);
 
 
 function myArguments(numOne, numTwo) {
     console.log("addition two numbers use function with arguments =>", numOne + numTwo);
 }
-myArguments(100, 200)
+myArguments(1500, 2312);
+
+
+function multiplication(c, d) {
+    console.log("multiplication two numbers use function with arguments =>", c * d);
+}
+multiplication(16, 3);
+
+function division(e, f) {
+    console.log("division two numbers use function with arguments =>", e / f);
+}
+division(8, 2);
