f6a4d815bb0bc61f4badc749113ff9437e83ba2a
yladev <yanlinnaung881@gmail.com>
2023-01-14 12:09:36 +0630

    update-rewrite code for global scope & functions

--- GlobalScope&Functions.js
+++ GlobalScope&Functions.js
@@ -2,14 +2,14 @@
 var myGlobal = 10;
 
 function fun1() {
+    // Assign 5 to oopsGlobal Here
     oopsGlobal = 5;
 }
-
 // Only change code above this line
 function fun2() {
     var output = "";
     if (typeof myGlobal != "undefined") {
-        output += "myGlobal: " + myGlobal;
+        output += "myGlobal: " + myGlobal; 
     }
     if (typeof oopsGlobal != "undefined") {
         output += " oopsGlobal: " + oopsGlobal;
