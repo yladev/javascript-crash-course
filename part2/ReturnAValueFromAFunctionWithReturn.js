475274d9c6530f5ddf18eca382b006841ec68752
yladev <yanlinnaung881@gmail.com>
2023-01-16 17:17:42 +0630

    update-return a value from a functions

---
+++ part2/ReturnAValueFromAFunctionWithReturn.js
@@ -0,0 +1,19 @@
+function minusSeven(num1) {
+    return num1 -7;
+}
+console.log("this is minusSeven =>", minusSeven(10));
+
+function multiplyFive(num2) {
+    return num2 * 7;
+}
+console.log("multiplyFive =>", multiplyFive(7));
+
+function addTwoNum(num3) {
+    return num3 + 15;
+}
+console.log("addTwoNum =>", addTwoNum(45));
+
+function dividedByFour(num4) {
+    return num4 / 4;
+}
+console.log("dividedByFour =>", dividedByFour(24));
