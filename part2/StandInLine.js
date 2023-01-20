eac1a419894d495fc191614cfe1b3bf7d7c8f11f
yladev <yanlinnaung881@gmail.com>
2023-01-20 16:47:55 +0630

    code-refactor

--- part2/StandInLine.js
+++ part2/StandInLine.js
@@ -3,7 +3,7 @@ function nextInLine(arr, item) {
     return arr.shift();
 }
 
-var testArr = [1, 2, 3, 4, 5];
+var testArr = [1,2,3,4,5];
 
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
