c46180c970a9621d9d7d5fde2aa72454c9349090
yladev <yanlinnaung881@gmail.com>
2023-01-21 11:29:52 +0630

    update-if statements

---
+++ part2/IfStatements.js
@@ -0,0 +1,16 @@
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "Yes, it's true";
+    }
+    return "No, it's false";
+}
+console.log(ourTrueOrFalse(true));
+
+
+function trueOrFalse(wasThatTrueOrFalse) {
+    if (wasThatTrueOrFalse) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+}
+console.log(trueOrFalse(false));
