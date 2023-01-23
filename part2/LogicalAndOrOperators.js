972cd4b5e16277e94058ff7771092b9288497fa1
yladev <yanlinnaung881@gmail.com>
2023-01-23 18:42:44 +0630

    update-logical And / Or operator

--- part2/LogicalAndOrOperators.js
+++ part2/LogicalAndOrOperators.js
@@ -1,1 +1,33 @@
-// Comprassion with the Logical And Operator
+// Comparison with the Logical And Operator
+function logicalAndOperator(myValue) {
+    if (myValue == 10 && myValue < 15) {
+        return true;
+    }
+    return false;
+}
+console.log("This is logical And operator =>", logicalAndOperator(10));
+
+function testLogicalAndOperator(Value) {
+    if (Value <= 10 && Value >= 15) {
+        return true;
+    }
+    return false;
+}
+console.log("This is logical And operator =>", testLogicalAndOperator(10));
+
+// Comparison with the Logical Or Operator
+function logicalOrOperator(myVal) {
+    if (myVal != "235" || myVal === 235) {
+        return true;
+    }
+    return false;
+}
+console.log("This is logical Or operator =>", logicalOrOperator(235));
+
+function testLogicalOrOperator(Val) {
+    if (Val === "400" || Val != 400) {
+        return true;
+    }
+    return false;
+}
+console.log("This is logical Or operator =>", testLogicalOrOperator(400));
