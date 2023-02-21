2a782c42027ceffe07c4c185762860b00d6aa8a4
yladev <yanlinnaung881@gmail.com>
2023-02-22 00:52:57 +0630

    update-dot notation

---
+++ part3/DotNotation.js
@@ -0,0 +1,11 @@
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
+
+console.log("testObj =>", hatValue);
+console.log("testObj =>", shirtValue);
