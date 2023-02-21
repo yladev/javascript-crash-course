58c676ff1a0ec087489c0db140ed29fa068c409c
yladev <yanlinnaung881@gmail.com>
2023-02-22 01:34:35 +0630

    update-bracket natation

---
+++ part3/BarcketNotation.js
@@ -0,0 +1,11 @@
+var bracketNotation = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water",
+};
+
+var entreeValue = bracketNotation["an entree"];
+var drinkValue = bracketNotation["the drink"];
+
+console.log("bracketNotation =>", entreeValue);
+console.log("bracketNotation =>", drinkValue);
